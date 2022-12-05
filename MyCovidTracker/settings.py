5456ba2a0eb4200d64a9f70a4b76135154fe8505
salimhass <salim.hassouneh@gmail.com>
2022-12-05 14:49:58 +0300

    database settings

--- MyCovidTracker/settings.py
+++ MyCovidTracker/settings.py
@@ -80,12 +80,15 @@ WSGI_APPLICATION = 'MyCovidTracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "covidtrackerdb",
+        "USER": "covidtrackerdb_user",
+        "PASSWORD": "OGETfDfG9cpB1yqldC0R9nrwHnshxPEc",
+        "HOST": "dpg-ce6tc3arrk071o6n8i30-a.frankfurt-postgres.render.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
